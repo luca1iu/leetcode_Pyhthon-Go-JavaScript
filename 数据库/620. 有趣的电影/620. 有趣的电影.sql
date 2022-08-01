ed3f55b8126c59029a9d5b99e10580930e8be0f6
Xianjing Liu <lucaliu217@outlook.com>
2022-08-01 15:32:40 +0800

    added 2236

---
+++ 数据库/620. 有趣的电影/620. 有趣的电影.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select * from cinema
+where id%2!=0 and description!="boring" 
+order by rating desc
+
+
+
+/* Write your PL/SQL query statement below */
+select * from cinema
+where mod(id,2)=1 and description <> 'boring' 
+order by rating desc
